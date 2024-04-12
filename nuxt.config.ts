38cec8600eeb80bada0cfe6ad88949caf1f9bbdb
Mateusz Gostański <mateusz.gostanski@gmail.com>
2024-04-12 21:29:52 +0200

    :heavy_plus_sign: feat: add i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/test-utils/module"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@nuxt/test-utils/module",
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    // Module Options
+    locales: ["pl", "en"],
+    defaultLocale: "pl",
+  },
 })
